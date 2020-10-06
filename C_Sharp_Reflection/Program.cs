6f1bbe5b273b8d2e29d46aa191986ec264f97418
gopalcb <gopal.cb@live.com>
2020-10-06 14:29:58 -0600

    first commit

---
+++ C_Sharp_Reflection/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Reflection
+{
+    class Program
+    {
+        private static Dictionary<string, string> _methodDictionary;
+        static void Main(string[] args)
+        {
+            _methodDictionary = new Dictionary<string, string>();
+            _methodDictionary = GetMethodsDictionary();
+
+            var type = typeof(StudentFunction);
+            var studentFunctionInstance = Activator.CreateInstance(type);
+
+            var testString = "Hello [GetName], your university name is [GetUniversity] and roll is [GetRoll]";
+            var match = Regex.Matches(testString, @"\[([A-Za-z0-9\-]+)]", RegexOptions.IgnoreCase);
+            foreach (var v in match)
+            {
+                var originalString = v.ToString();
+                var x = v.ToString();
+                x = x.Replace("[", "");
+                x = x.Replace("]", "");
+                x = _methodDictionary[x];
+
+                var toInvoke = type.GetMethod(x);
+                var result = toInvoke.Invoke(studentFunctionInstance, null);
+                testString = testString.Replace(originalString, result.ToString());
+            }
+
+            Console.WriteLine(testString);
+
+            Console.ReadKey();
+        }
+
+        private static Dictionary<string, string> GetMethodsDictionary()
+        {
+            var dictionary = new Dictionary<string, string>
+        {
+            {"GetName", "GetName"},
+            {"GetUniversity", "GetUniversity"},
+            {"GetRoll","GetRoll"}
+        };
+            return dictionary;
+        }
+    }
+}
---
+++ C_Sharp_Reflection/StudentFunction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Reflection
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int Roll { get; set; }
+    }
+
+    public class StudentFunction
+    {
+        private Student student;
+        public StudentFunction()
+        {
+            student = new Student
+            {
+                Name = "Gopal C. Bala",
+                University = "Jahangirnagar University",
+                Roll = 1424
+            };
+        }
+
+        public string GetName()
+        {
+            return student.Name;
+        }
+
+        public string GetUniversity()
+        {
+            return student.University;
+        }
+
+        public int GetRoll()
+        {
+            return student.Roll;
+        }
+    }
+}
